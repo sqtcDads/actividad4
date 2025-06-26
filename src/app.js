019e5fe85db136d96334ee3d648cb584b9b45c61
ricardo sanchez <snchzg93@gmail.com>
2025-06-26 14:06:01 -0400

    packages, gitignore y app

---
+++ src/app.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { createServer } from 'http';
+import userRouter from './routes/users.js';
+import viewsRouter from './routes/views.js';
+import sessionRouter from './routes/session.js';
+import bodyParser from 'body-parser'
+import { engine } from 'express-handlebars'
+import mongoose from 'mongoose'
+import config from './config/index.js';
+import { initPassport } from "./middleware/passport.js"
+import passport from 'passport';
+import session from 'express-session';
+
+
+const app = express()
+const server = createServer(app)
+const { PORT, MONGO_URI } = config
+
+//hbs
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './src/views')
+
+//Middleware
+app.use(express.json())
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+app.set('trust proxy', 1)
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+initPassport()
+app.use("/", viewsRouter)
+app.use("/api/users", userRouter)
+app.use("/api/session", sessionRouter)
+
+//endpoint
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+mongoose.connect(MONGO_URI, { dbName: "integrative_practice" })
+    .then(() => console.log("mongodb conectado"))
+    .catch((err) => {
+        console.error({ error: err.message })
+        process.exit(1)
+    })
